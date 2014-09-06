2f69e1b143c0aecd880ef0a41952ec2f95c1f2a1
Gregory Vorobyev <gvorobjev@gmail.com>
2014-09-06 13:40:42 -0400

    Translation repository

---
+++ Gopher/Gopher.Model/Abstractions/ITranslationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gopher.Model.Abstractions.Core;
+
+namespace Gopher.Model.Abstractions
+{
+    public interface ITranslationRepository
+    {
+        string GetTranslation(string label, string lang);
+        IEnumerable<string> GetAll(string lang);
+    }
+}
---
+++ Gopher/Gopher.Model/Repositories/TranslationRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gopher.Model.Abstractions;
+
+namespace Gopher.Model.Repositories
+{
+    public class TranslationRepository : ITranslationRepository
+    {
+        public string GetTranslation(string label, string lang)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetAll(string lang)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
