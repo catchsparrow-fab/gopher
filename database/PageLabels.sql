4efa2d74b11d77a775c5f3556a1dc76df750c1bb
Gregory Vorobyev <gvorobjev@gmail.com>
2014-09-14 23:25:20 -0400

    Added labels to db

--- database/PageLabels.sql
+++ database/PageLabels.sql
@@ -5,5 +5,15 @@ INSERT INTO Languages (LanguageId, LanguageName) VALUES (2, 'Japanese')
 
 GO
 
-AddTranslation 'Languages_English', 'English'
-AddTranslation 'Languages_Japanese', 'Japanese'
+exec AddTranslation 'Languages_English', 'English'
+exec AddTranslation 'Languages_Japanese', 'Japanese'
+
+exec AddTranslation 'Header_CustomerSearch', 'Customer search'
+exec AddTranslation 'Header_Import', 'Import'
+exec AddTranslation 'Header_Administration', 'Administration'
+exec AddTranslation 'Header_Greeting', 'Hello'
+exec AddTranslation 'Header_LogOff', 'Log off'
+
+exec AddTranslation 'Administration_Tabs_Users', 'Users'
+exec AddTranslation 'Administration_Tabs_Codes', 'Codes'
+exec AddTranslation 'Administration_Tabs_Translations', 'Translations'
